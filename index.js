b2159daadc0473bbc40adf8bd1dd470729471854
Etienne Robert <etiennerobert33@gmail.com>
2025-12-03 17:26:17 +0100

    Add game loop with update and render stubs

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const canvas = document.querySelector("canvas");
-console.log(canvas);
+
+let lastTime = 0;
+
+function render() {
+  // TODO: Implement
+  console.log("Rendering screen");
+}
+
+function updateState(deltaTime) {
+  // TODO: Implement
+  console.log(deltaTime + " elapsed");
+}
+
+function gameLoop(currentTime) {
+  const deltaTime = currentTime - lastTime;
+  lastTime = currentTime;
+  updateState(deltaTime);
+  render();
+  requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
