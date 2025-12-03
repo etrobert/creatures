fd7c78850ea81df884364fcafc7113ac6f1a5a07
Etienne Robert <etiennerobert33@gmail.com>
2025-12-03 17:43:55 +0100

    Add null checks and draw green rectangle in render

--- src/render.ts
+++ src/render.ts
@@ -1,6 +1,14 @@
-const canvas = document.querySelector("canvas");
+const canvasOr = document.querySelector("canvas");
+if (canvasOr === null) throw new Error("Could not get canvas");
+const canvas = canvasOr;
+
+const ctxOr = canvas.getContext("2d");
+if (ctxOr === null) throw new Error("Could not get ctx");
+const ctx = ctxOr;
 
 export function render() {
   // TODO: Implement
-  console.log("Rendering screen");
+  ctx.fillStyle = "green";
+  // Add a rectangle at (10, 10) with size 100x100 pixels
+  ctx.fillRect(10, 10, 100, 100);
 }
