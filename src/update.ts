839708d76a4df131c2ac7de85814f83b00239067
Palomia <amaury.palomino@outlook.com>
2025-12-17 16:10:08 +0100

    Change the state to a very much more efficient solution that the god of TS wouldnt have found

--- src/update.ts
+++ src/update.ts
@@ -1,7 +1,7 @@
 import type { State } from "./state.js";
 
-export function update(state: State, deltaTime: number) {
+export function update(state: State, deltaTime: number): State {
   console.log(deltaTime + " elapsed");
-  const newPosition = { x: state.position.x + 1, y: state.position.y };
+  const newPosition = { ...state.position, x: state.position.x + 1 };
   return { position: newPosition };
 }
