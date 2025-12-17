b35851d17175c81fc997a1b2650ca452f5b45eee
Etienne Robert <etiennerobert33@gmail.com>
2025-12-17 18:50:23 +0100

    Clear nextAction after movement update

--- src/update.ts
+++ src/update.ts
@@ -1,27 +1,31 @@
 import type { Creature, State } from "./state.js";
 
-const updatePosition = (creature: Creature) => {
+const updatePosition = (creature: Creature): Creature => {
   if (creature.nextAction === null) return creature;
 
   switch (creature.nextAction.direction) {
     case "up":
       return {
         ...creature,
+        nextAction: null,
         position: { ...creature.position, y: creature.position.y - 1 },
       };
     case "left":
       return {
         ...creature,
+        nextAction: null,
         position: { ...creature.position, x: creature.position.x - 1 },
       };
     case "right":
       return {
         ...creature,
+        nextAction: null,
         position: { ...creature.position, x: creature.position.x + 1 },
       };
     case "down":
       return {
         ...creature,
+        nextAction: null,
         position: { ...creature.position, y: creature.position.y + 1 },
       };
   }
