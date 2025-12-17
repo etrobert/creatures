2036fa4907734443dc0cc65e1d8c6f9db0699d99
Etienne Robert <etiennerobert33@gmail.com>
2025-12-17 16:59:17 +0100

    Refactor creature creation signature and usage

--- src/state.ts
+++ src/state.ts
@@ -1,11 +1,14 @@
 export const createState = () => ({
-  creatures: [createCreature(), createCreature(1, { x: 10, y: 100 })],
+  creatures: [
+    createCreature(),
+    createCreature({ player: 1, position: { x: 10, y: 100 } }),
+  ],
 });
 
 export type State = ReturnType<typeof createState>;
 
-const createCreature = (player = 0, position = { x: 0, y: 0 }) => ({
-  position,
-  health: 10,
+const createCreature = ({ player = 0, position = { x: 0, y: 0 } } = {}) => ({
   player,
+  health: 10,
+  position,
 });
