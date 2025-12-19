3cc3dfddacbeee146821a545a2fcf22356c947a2
Étienne Robert <etiennerobert33@gmail.com>
2025-12-19 17:52:45 +0100

    Switch from deltaTime to tick-based updates (#2)

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,10 @@ import { update } from "./update.js";
 import { setupEventListeners } from "./input.js";
 import { setState, state } from "./state.js";
 
-let lastTime = 0;
-
 setupEventListeners();
 
 function gameLoop(currentTime: number) {
-  const deltaTime = currentTime - lastTime;
-  lastTime = currentTime;
-  setState(update(state, deltaTime));
+  setState(update(state, currentTime));
   render(state);
   requestAnimationFrame(gameLoop);
 }
