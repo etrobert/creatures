935f108725dca7ff983ccdce629cbc6839dc5597
Palomia <amaury.palomino@outlook.com>
2025-12-21 12:26:24 +0100

    Movement based on click

--- src/input.ts
+++ src/input.ts
@@ -1,6 +1,6 @@
 import { setState, state } from "./state.js";
 import { canvas, cellWidth, cellHeight } from "./render.js";
-import type { Position } from "./state.js";
+import type { Creature, Position } from "./state.js";
 
 export const setupEventListeners = () => {
   window.addEventListener("keydown", (event) => {
@@ -42,7 +42,52 @@ export const setupEventListeners = () => {
       const { x, y } = canvasOnPosition({ x: event.offsetX, y: event.offsetY });
       console.log(x);
       console.log(y);
+      if (x === undefined) return;
+      const activeCreature = state.creatures.find((creature) =>
+        findActiveCreature(creature, 0),
+      );
+      // add ghost calculation when in
+      if (activeCreature === undefined) return;
+      const nextActionsX =
+        activeCreature?.position.x < x
+          ? new Array(x - activeCreature?.position.x).fill({
+              type: "move",
+              direction: "right",
+            })
+          : new Array(activeCreature?.position.x - x).fill({
+              type: "move",
+              direction: "left",
+            });
+      const nextActionsY =
+        activeCreature?.position.y < y
+          ? new Array(y - activeCreature?.position.y).fill({
+              type: "move",
+              direction: "down",
+            })
+          : new Array(activeCreature?.position.y - y).fill({
+              type: "move",
+              direction: "up",
+            });
+      const nextActions = [...nextActionsX, ...nextActionsY];
+      setState({
+        ...state,
+        creatures: state.creatures.map((creature) =>
+          creature.player === 0 && creature.selected
+            ? {
+                ...creature,
+                nextActions: [
+                  // ...creature.nextActions, to be added when ghost is there
+                  ...nextActions,
+                ],
+              }
+            : creature,
+        ),
+      });
     });
+};
+
+const findActiveCreature = (creature: Creature, player: number) => {
+  return creature.player === player;
 };
 
 // translation bwtween grid position and canvas position
