d60008ee3374188c4d503e6929f38ef2d6db530b
Etienne Robert <etiennerobert33@gmail.com>
2025-12-21 18:44:41 +0100

    Add express server with hello world route to server app

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,5 +1,15 @@
 import { j } from "@creatures/shared/test";
+import express from "express";
 
-console.log("I am the server");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
 
 console.log(" This is coming from shared: " + j);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}`);
+});
