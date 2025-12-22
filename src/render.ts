f7c8407f4e58ee772182fb835843dd152bab4cd0
Palomia <132281378+Palomia@users.noreply.github.com>
2025-12-22 14:33:57 +0100

    Move renderCreature to its own file (#14)

--- src/render.ts
+++ src/render.ts
@@ -2,6 +2,7 @@ import { countColumns, countRow, type Creature, type State } from "./state.js";
 import type { Direction, Position } from "./state.js";
 import { tickDuration, updatePosition, collisionWithMap } from "./update.js";
 import { renderBackground, backgroundMap } from "./background.js";
+import { renderCreature } from "./renderCreature.js";
 
 const getCanvas = () => {
   const canvas = document.querySelector("canvas");
@@ -25,49 +26,10 @@ canvas.width = cellWidth * (countColumns + 1); //The canvas is 1 cell bigger bec
 canvas.height = cellHeight * (countRow + 1);
 
 // translation bwtween grid position and canvas position
-const gridToCanvas = ({ x, y }: Position) => ({
+export const gridToCanvas = ({ x, y }: Position) => ({
   x: x * cellWidth + cellWidth / 2,
   y: y * cellHeight + cellHeight / 2,
 });
-
-const getDirectionLine = (direction: Direction) => {
-  switch (direction) {
-    case "down":
-      return 0;
-    case "right":
-      return 2;
-    case "left":
-      return 6;
-    case "up":
-      return 4;
-  }
-};
-
-const img = new Image();
-const imgWidth = 40;
-const imgHeight = 40;
-const animationFrames = 6;
-const frameDuration = tickDuration / animationFrames;
-img.src = "./sprites/animations/bulbasaur/Walk-Anim.png";
-
-const renderCreature = (creature: Creature, currentTime: number) => {
-  const color = creature.player === 0 ? "blue" : "red";
-  const canvasPosition = gridToCanvas(creature.position);
-  ctx.fillStyle = color;
-  ctx.fillRect(canvasPosition.x, canvasPosition.y, cellWidth, cellHeight);
-
-  ctx.drawImage(
-    img,
-    (Math.floor(currentTime / frameDuration) % animationFrames) * imgWidth,
-    getDirectionLine(creature.direction) * imgHeight,
-    imgWidth,
-    imgHeight,
-    canvasPosition.x - (imgWidth - cellWidth) / 2,
-    canvasPosition.y - (imgWidth - cellWidth) / 2,
-    imgWidth,
-    imgHeight,
-  );
-};
 
 export const render = (state: State, currentTime: number) => {
   ctx.fillStyle = "lightskyblue";
---
+++ src/renderCreature.ts
@@ -0,0 +1,43 @@
+import { type Creature, type Position, type Direction } from "./state.js";
+import { ctx } from "./render.js";
+import { cellHeight, cellWidth, gridToCanvas } from "./render.js";
+import { tickDuration } from "./update.js";
+
+const getDirectionLine = (direction: Direction) => {
+  switch (direction) {
+    case "down":
+      return 0;
+    case "right":
+      return 2;
+    case "left":
+      return 6;
+    case "up":
+      return 4;
+  }
+};
+
+const img = new Image();
+const imgWidth = 40;
+const imgHeight = 40;
+const animationFrames = 6;
+const frameDuration = tickDuration / animationFrames;
+img.src = "./sprites/animations/bulbasaur/Walk-Anim.png";
+
+export const renderCreature = (creature: Creature, currentTime: number) => {
+  const color = creature.player === 0 ? "blue" : "red";
+  const canvasPosition = gridToCanvas(creature.position);
+  ctx.fillStyle = color;
+  ctx.fillRect(canvasPosition.x, canvasPosition.y, cellWidth, cellHeight);
+
+  ctx.drawImage(
+    img,
+    (Math.floor(currentTime / frameDuration) % animationFrames) * imgWidth,
+    getDirectionLine(creature.direction) * imgHeight,
+    imgWidth,
+    imgHeight,
+    canvasPosition.x - (imgWidth - cellWidth) / 2,
+    canvasPosition.y - (imgWidth - cellWidth) / 2,
+    imgWidth,
+    imgHeight,
+  );
+};
