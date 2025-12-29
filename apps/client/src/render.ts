b70465e4daf0bfc7c2c0e77409316af099097a21
Palomia <amaury.palomino@outlook.com>
2025-12-29 13:13:59 +0100

    Create render UI and move health bar there

--- apps/client/src/render.ts
+++ apps/client/src/render.ts
@@ -9,6 +9,7 @@ import {
   type State,
   type Creature,
 } from "@creatures/shared/state";
+import { renderCreatureHealth } from "./renderUI.js";
 
 const getCanvas = () => {
   const canvas = document.querySelector("canvas");
@@ -62,40 +63,6 @@ export const render = (state: State, currentTime: number) => {
       ctx.globalAlpha = 1;
     }
   }
-};
-
-const renderCreatureHealth = (creature: Creature) => {
-  const canvasPosition = gridToCanvas(creature.position);
-  const xPad = cellWidth / 8;
-  const yPadTop = (-1 * cellHeight) / 16;
-  const yPadBot = (cellHeight * 15) / 16;
-  ctx.fillStyle = "black";
-
-  ctx.fillRect(
-    canvasPosition.x + xPad - 1,
-    canvasPosition.y + yPadTop - 1,
-    cellWidth - 2 * xPad + 2,
-    cellHeight - (yPadTop + yPadBot) + 2,
-  );
-  ctx.fillStyle = "LightGray";
-
-  ctx.fillRect(
-    canvasPosition.x + xPad,
-    canvasPosition.y + yPadTop,
-    cellWidth - 2 * xPad,
-    cellHeight - (yPadTop + yPadBot),
-  );
-
-  const percentageHealth = creature.health / creature.maxHealth;
-  ctx.fillStyle = "green";
-  const endCurrentHP = percentageHealth * (cellWidth - 2 * xPad);
-
-  ctx.fillRect(
-    canvasPosition.x + xPad,
-    canvasPosition.y + yPadTop,
-    endCurrentHP,
-    cellHeight - (yPadTop + yPadBot),
-  );
 };
 
 export const getGhost = (creature: Creature) => {
