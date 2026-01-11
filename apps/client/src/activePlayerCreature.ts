873dd46646b2fdf99e254615d6a96e9c4d121046
Palomia <amaury.palomino@outlook.com>
2026-01-11 13:25:54 +0100

    Correct bug initialize without creatures in the bicket of activable creature for main player

--- apps/client/src/activePlayerCreature.ts
+++ apps/client/src/activePlayerCreature.ts
@@ -8,14 +8,7 @@ export const listPlayerCreaturesId = (player: number) => {
     .map((creature) => creature.id);
 };
 
-const getInitialActiveCreatureId = () => {
-  const activePlayerCreaturesId = listPlayerCreaturesId(activePlayer);
-  const activeCreatureId = activePlayerCreaturesId.at(0);
-  if (activeCreatureId === undefined)
-    throw new Error("Could not get active creature");
-  return activeCreatureId;
-};
-export let activeCreatureId = getInitialActiveCreatureId();
+export let activeCreatureId = "0";
 
 export const setActiveCreatureId = (newActiveCreatureId: string) =>
   (activeCreatureId = newActiveCreatureId);
