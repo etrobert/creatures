c7c6391865ab38bfc7dd66a12dc91a4bfcb16cef
Etienne Robert <etiennerobert33@gmail.com>
2026-01-12 18:24:10 +0100

    remove entity check before renderEntity call

--- apps/client/src/render.ts
+++ apps/client/src/render.ts
@@ -60,8 +60,7 @@ export const render = (state: State, currentTime: number) => {
       const entities = state.entities.filter(
         (entity) => entity.position.x === x && entity.position.y === y,
       );
-      if (entities[0])
-        entities.forEach((entity) => renderEntity(entity, currentTime));
+      entities.forEach((entity) => renderEntity(entity, currentTime));
       const creatureGhosts = state.entities
         .filter(isCreature)
         .filter((creature) => creature.nextActions.length !== 0)
