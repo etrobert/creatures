8906780a110dca3e812cb4917b87336ef40dc6d8
Etienne Robert <etiennerobert33@gmail.com>
2026-01-12 18:15:03 +0100

    rename updateCreature to updateEntity module

--- apps/server/src/update.ts
+++ apps/server/src/update.ts
@@ -1,5 +1,5 @@
 import { isCreature, type State } from "@creatures/shared/state";
-import { updateEntity } from "./updateCreature.js";
+import { updateEntity } from "./updateEntity.js";
 
 export function update(state: State): State {
   for (let entity of state.entities) state = updateEntity(state, entity);
