64067897aea4503e614a94d158d80a6c93c4bfed
Palomia <amaury.palomino@outlook.com>
2026-01-13 10:47:20 +0100

    add an accessKit function

--- packages/shared/creaturePool.ts
+++ packages/shared/creaturePool.ts
@@ -1,5 +1,10 @@
 import { bulbizard } from "./creatures/bulbizard";
+import { type CreatureKit, type CreatureName } from "./state";
 
-export const creaturePoolKits = {
-  bulbizard,
+export const accessKit = (creatureName: CreatureName) => {
+  creaturePoolKits.filter(
+    (creatureKit: CreatureKit) => creatureKit.type === creatureName,
+  );
 };
+
+const creaturePoolKits = [bulbizard];
