ee9348897c45ef49abcac363391517c9b061781b
Palomia <132281378+Palomia@users.noreply.github.com>
2026-01-22 14:04:40 +0100

    add colors (#40)
    * add colors
    
    * dead code is killed
    
    * the changes

--- apps/client/src/renderCreatureHealth.ts
+++ apps/client/src/renderCreatureHealth.ts
@@ -1,7 +1,21 @@
 import type { Creature } from "@creatures/shared/state";
 import { cellHeight, cellWidth, ctx, gridToCanvas } from "./render.js";
+import { activeCreatureId, activePlayer } from "./activePlayerCreature.js";
+
+const getHealthColor = (creature: Creature) => {
+  const enemyColor = "#EF3E4D"; //red
+  const activeCreatureColor = "#40A060"; //dark green
+  const allyColor = "#50C878"; // light green
+  if (creature.player !== activePlayer) return enemyColor;
+  return creature.id === activeCreatureId ? activeCreatureColor : allyColor;
+};
 
 export const renderCreatureHealth = (creature: Creature) => {
+  const color = getHealthColor(creature);
+  renderCreatureHealthWithColor(creature, color);
+};
+
+const renderCreatureHealthWithColor = (creature: Creature, color: string) => {
   const canvasPosition = gridToCanvas(creature.position);
   const xPad = cellWidth / 8;
   const yPadTop = (-1 * cellHeight) / 16;
@@ -24,7 +38,7 @@ export const renderCreatureHealth = (creature: Creature) => {
   );
 
   const percentageHealth = creature.health / creature.maxHealth;
-  ctx.fillStyle = "green";
+  ctx.fillStyle = color;
   const endCurrentHP = percentageHealth * (cellWidth - 2 * xPad);
 
   ctx.fillRect(
