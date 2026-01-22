8c689e86c7a559921033962e9bf3d7cfe93aa172
Palomia <amaury.palomino@outlook.com>
2026-01-22 22:57:34 +0100

    creature out of list and update only living creatures
    todo: update active creature id so the grieving player doesn't request action from dead creature

--- apps/server/src/update.ts
+++ apps/server/src/update.ts
@@ -2,7 +2,9 @@ import { type State } from "@creatures/shared/state";
 import { updateEntity } from "./updateEntity.js";
 
 export function update(state: State): State {
-  for (let entity of state.entities) state = updateEntity(state, entity.id);
-
+  for (let entity of state.entities) {
+    if (state.entities.some((thisEntity) => thisEntity.id === entity.id))
+      state = updateEntity(state, entity.id);
+  }
   return state;
 }
