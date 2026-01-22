6d0038f6ef56d6339c80502d23c3712e9a53f86e
Etienne Robert <etiennerobert33@gmail.com>
2026-01-22 12:33:13 +0100

    Refactor input event listeners (#39)

--- apps/client/src/input.ts
+++ apps/client/src/input.ts
@@ -50,105 +50,111 @@ const createAction = (creature: Creature, actionType: ActionType): Action => {
   }
 };
 
+function createActionOnKeyDown(event: KeyboardEvent) {
+  if (state === undefined) throw new Error("state is undefined");
+  const activeCreature = findActiveCreature(state, activeCreatureId);
+
+  const getAction = () => {
+    switch (event.code) {
+      case "ArrowUp":
+        return { type: "move", direction: "up" } as const;
+      case "ArrowLeft":
+        return { type: "move", direction: "left" } as const;
+      case "ArrowDown":
+        return { type: "move", direction: "down" } as const;
+      case "ArrowRight":
+        return { type: "move", direction: "right" } as const;
+      case "KeyQ":
+        return createAction(
+          activeCreature,
+          getCreatureKit(activeCreature.name).actionQ,
+        );
+      case "KeyW":
+        return createAction(
+          activeCreature,
+          getCreatureKit(activeCreature.name).actionW,
+        );
+      case "KeyE":
+        return createAction(
+          activeCreature,
+          getCreatureKit(activeCreature.name).actionE,
+        );
+    }
+  };
+
+  const newAction = getAction();
+
+  if (newAction === undefined) return;
+
+  sendClientMessage({
+    type: "player input",
+    creatureId: activeCreature.id,
+    actions: [newAction],
+  });
+}
+
+function onCanvasClick(event: PointerEvent) {
+  if (state === undefined) throw new Error("state is undefined");
+  const { x, y } = canvasToGrid({ x: event.offsetX, y: event.offsetY });
+  const activeCreature = state.entities
+    .filter(isCreature)
+    .find((creature) => creature.id === activeCreatureId);
+  // add ghost calculation when in
+  if (activeCreature === undefined) return;
+  const ghost = getGhost(activeCreature);
+  const nextActionsX =
+    ghost.position.x < x
+      ? new Array(x - ghost.position.x).fill({
+          type: "move",
+          direction: "right",
+        })
+      : new Array(ghost.position.x - x).fill({
+          type: "move",
+          direction: "left",
+        });
+  const nextActionsY =
+    ghost.position.y < y
+      ? new Array(y - ghost.position.y).fill({
+          type: "move",
+          direction: "down",
+        })
+      : new Array(ghost.position.y - y).fill({
+          type: "move",
+          direction: "up",
+        });
+  sendClientMessage({
+    type: "player input",
+    creatureId: activeCreature.id,
+    actions: [...nextActionsX, ...nextActionsY],
+  });
+}
+
+function changeActiveCreatureOnKeyDown(event: KeyboardEvent) {
+  const getActiveCreature = () => {
+    const listActivePlayerCreatureIds = listPlayerCreatureIds(activePlayer);
+    switch (event.code) {
+      case "Digit1":
+        return listActivePlayerCreatureIds[0];
+      case "Digit2":
+        return listActivePlayerCreatureIds[1];
+      case "Digit3":
+        return listActivePlayerCreatureIds[2];
+      case "Digit4":
+        return listActivePlayerCreatureIds[3];
+    }
+  };
+
+  const newActiveCreature = getActiveCreature();
+  if (newActiveCreature === undefined) return;
+  setActiveCreatureId(newActiveCreature);
+}
+
 export const setupEventListeners = () => {
-  window.addEventListener("keydown", (event) => {
-    if (state === undefined) throw new Error("state is undefined");
-    const activeCreature = findActiveCreature(state, activeCreatureId);
+  window.addEventListener("keydown", createActionOnKeyDown);
 
-    const getAction = () => {
-      switch (event.code) {
-        case "ArrowUp":
-          return { type: "move", direction: "up" } as const;
-        case "ArrowLeft":
-          return { type: "move", direction: "left" } as const;
-        case "ArrowDown":
-          return { type: "move", direction: "down" } as const;
-        case "ArrowRight":
-          return { type: "move", direction: "right" } as const;
-        case "KeyQ":
-          return createAction(
-            activeCreature,
-            getCreatureKit(activeCreature.name).actionQ,
-          );
-        case "KeyW":
-          return createAction(
-            activeCreature,
-            getCreatureKit(activeCreature.name).actionW,
-          );
-        case "KeyE":
-          return createAction(
-            activeCreature,
-            getCreatureKit(activeCreature.name).actionE,
-          );
-      }
-    };
+  canvas.addEventListener("click", onCanvasClick);
 
-    const newAction = getAction();
-
-    if (newAction === undefined) return;
-
-    sendClientMessage({
-      type: "player input",
-      creatureId: activeCreature.id,
-      actions: [newAction],
-    });
-  });
-
-  canvas.addEventListener("click", (event) => {
-    if (state === undefined) throw new Error("state is undefined");
-    const { x, y } = canvasToGrid({ x: event.offsetX, y: event.offsetY });
-    const activeCreature = state.entities
-      .filter(isCreature)
-      .find((creature) => creature.id === activeCreatureId);
-    // add ghost calculation when in
-    if (activeCreature === undefined) return;
-    const ghost = getGhost(activeCreature);
-    const nextActionsX =
-      ghost.position.x < x
-        ? new Array(x - ghost.position.x).fill({
-            type: "move",
-            direction: "right",
-          })
-        : new Array(ghost.position.x - x).fill({
-            type: "move",
-            direction: "left",
-          });
-    const nextActionsY =
-      ghost.position.y < y
-        ? new Array(y - ghost.position.y).fill({
-            type: "move",
-            direction: "down",
-          })
-        : new Array(ghost.position.y - y).fill({
-            type: "move",
-            direction: "up",
-          });
-    sendClientMessage({
-      type: "player input",
-      creatureId: activeCreature.id,
-      actions: [...nextActionsX, ...nextActionsY],
-    });
-  });
-
-  window.addEventListener("keydown", (event) => {
-    const getActiveCreature = () => {
-      const listActivePlayerCreatureIds = listPlayerCreatureIds(activePlayer);
-      switch (event.code) {
-        case "Digit1":
-          return listActivePlayerCreatureIds[0];
-        case "Digit2":
-          return listActivePlayerCreatureIds[1];
-        case "Digit3":
-          return listActivePlayerCreatureIds[2];
-        case "Digit4":
-          return listActivePlayerCreatureIds[3];
-      }
-    };
-
-    const newActiveCreature = getActiveCreature();
-    if (newActiveCreature === undefined) return;
-    setActiveCreatureId(newActiveCreature);
-  });
+  window.addEventListener("keydown", changeActiveCreatureOnKeyDown);
 };
 
 // translation bwtween grid position and canvas position
